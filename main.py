d0dc5e9292ee6f47871e3c18a05949c9b1a094f8
Alfons Nilsson <alfonsnilsson@Alfonss-MacBook-Air.local>
2023-05-24 14:27:31 +0200

    final

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def searchNode(node, searchedNodes):
 
   return False
 
-def generateGrid(nodes, p):
+def generateGrid(nodes, p, diagonals = 0):
   # Create grid of nodes
   for i in range(size):
     for j in range(size):
@@ -66,9 +66,45 @@ def generateGrid(nodes, p):
         a.connectedEdges.append(edge)
         b.connectedEdges.append(edge)
 
+  if diagonals >= 1:
+    for i in range(size - 1):
+      for j in range(size - 1):
+        if keepEdge(p):
+          a = nodes[i + j * size]
+          b = nodes[i + 1 + (j + 1) * size]
+          edge = (a, b)
+
+          a.connectedEdges.append(edge)
+          b.connectedEdges.append(edge)
+
+  if diagonals >= 2:
+    for i in range(size - 1):
+      for j in range(size - 1):
+        if keepEdge(p):
+          a = nodes[i + 1 + j * size]
+          b = nodes[i + (j + 1) * size]
+          edge = (a, b)
+
+          a.connectedEdges.append(edge)
+          b.connectedEdges.append(edge)
+
+  # # Draw nodes
+  # X = list(map(lambda n: n.x, nodes))
+  # Y = list(map(lambda n: n.y, nodes))
+  # plt.scatter(X, Y)
+
+  # # Draw edges
+  # for node in nodes:
+  #   for edge in node.connectedEdges:
+  #     X = [ edge[0].x, edge[1].x ]
+  #     Y = [ edge[0].y, edge[1].y ]
+  #     plt.plot(X, Y)
+
+  # plt.show()
+
 def simulate(p):
   nodes = []
-  generateGrid(nodes, p)
+  generateGrid(nodes, p, diagonals)
 
   # Remove edges - Awefully slow :(
   # for edge in edges[:]:
@@ -98,30 +134,61 @@ def getProbability(p):
   return successfullSimulations / iterations
 
 n = 10
-pStart = 0.3
-pStop = 0.7
-pSteps = 100
-iterations = 5000
+pStart = 0
+pStop = 1
+pSteps = 300
+iterations = 500
+diagonals = 0
 
 size = n * 2 + 1
 
 if __name__ == '__main__':
+  plt.title("n = " + str(n) + ", iters/p = " + str(iterations))
+  plt.xlabel("p = ℙ(behåll kant)")
+  plt.ylabel("ℙ(väg existerar från vänster till höger)")
+
   P = []
   PROB = []
 
   step = 0
   for p in np.linspace(pStart, pStop, pSteps):
-  # p = 0.2
     print("Step", step + 1)
     prob = getProbability(p)
     P.append(p)
     PROB.append(prob)
     step += 1
 
-  plt.title("n = " + str(n) + ", iters/p = " + str(iterations))
-  plt.xlabel("p")
-  plt.ylabel("propability that a path from left to right exists")
-  plt.plot(P, PROB)
+  plt.plot(P, PROB, label="$B_n$")
+
+  diagonals = 1
+
+  P = []
+  PROB = []
+
+  step = 0
+  for p in np.linspace(pStart, pStop, pSteps):
+    print("Step", step + 1)
+    prob = getProbability(p)
+    P.append(p)
+    PROB.append(prob)
+    step += 1
+
+  plt.plot(P, PROB, label="$T_n$")
+
+  diagonals = 2
+
+  P = []
+  PROB = []
+
+  step = 0
+  for p in np.linspace(pStart, pStop, pSteps):
+    print("Step", step + 1)
+    prob = getProbability(p)
+    P.append(p)
+    PROB.append(prob)
+    step += 1
+
+  plt.plot(P, PROB, label="$D_n$")
 
   # X = np.linspace(pStart, pStop, 500)
   # Y = norm.cdf(20 * (X - 0.5))
@@ -132,22 +199,19 @@ if __name__ == '__main__':
 
   # print("mu, std", norm.fit(GRAD))
 
+  plt.axvline(x=1/2, linewidth=1, color='darkblue')
+  plt.text(1/2, -0.11, '$\\frac{1}{2}$', rotation=0, horizontalalignment="center", weight="bold")
+
+  plt.axvline(x=1/3, linewidth=1, color='red')
+  plt.text(1/3, -0.11, '$\\frac{1}{3}$', rotation=0, horizontalalignment="center", weight="bold")
+
+  plt.axvline(x=1/4, linewidth=1, color='green')
+  plt.text(1/4, -0.11, '$\\frac{1}{4}$', rotation=0, horizontalalignment="center", weight="bold")
+
+  # plt.subplots_adjust(bottom=0.2)
+
+  # txt="iters/p = " + str(iterations)
+  # plt.figtext(0.5, 0.09, txt, wrap=True, horizontalalignment='center', verticalalignment="top")
+
   plt.legend()
   plt.show()
-
-
-
-
-
-# # Draw nodes
-# X = list(map(lambda n: n.x, nodes))
-# Y = list(map(lambda n: n.y, nodes))
-# plt.scatter(X, Y)
-
-# # Draw edges
-# for edge in edges:
-#   X = [ edge[0].x, edge[1].x ]
-#   Y = [ edge[0].y, edge[1].y ]
-#   plt.plot(X, Y)
-
-# plt.show()
